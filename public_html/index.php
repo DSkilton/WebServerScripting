ee4c7f7fd751eb2bf73e5c3babab0a699d60186a
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2022-01-27 11:26:32 +0000

    removed white space

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,7 @@
         <meta charset="windows-1252">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
-        <!--Bootstrap CSS -->
+        <!--BootstrapCSS-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" crossorigin="anonymous">
         
         <!--Boostrap JavaScript-->
